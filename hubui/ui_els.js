079efb17f636fdc0f027becd51c32361a7b5f7b0
CrazyMax <127091629+Crazy-Max-Blog@users.noreply.github.com>
2024-06-02 21:26:06 +0400

    Update ui_els.js

--- hubui/ui_els.js
+++ hubui/ui_els.js
@@ -1,6 +1,5 @@
 UI_Els = {
   "btn": '`<button class="uibtn" onclick=\'${EF}("${data["id"]}")\'>${data["text"]}</button>`',
-  "btn": '`<button class="uibtn" onclick=\'${EF}("${data["id"]}")\'>${data["text"]}</button>`',
   "text": '`<div onclick=\'${EF}("${data["id"]}")\'>${data["text"]}</div>`',
   "col": 'var elsj=data["text"];var els="";for(let i=0;i<elsj.length;i++){elsj[i]["p"]="0";els+=this.addElHtml(JSON.stringify(elsj[i])).outerHTML};`<div style="width:100%;display:flex" onclick=\'${EF}("${data["id"]}")\'>${els}</div>`',
 }
