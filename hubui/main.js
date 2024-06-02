dd94f522e07f6cb491c161831087bdace0d51994
CrazyMax <127091629+Crazy-Max-Blog@users.noreply.github.com>
2024-06-02 19:54:07 +0400

    Add files via upload

--- hubui/main.js
+++ hubui/main.js
@@ -15,14 +15,19 @@ class HubUI {
     data=JSON.parse(data);
     var dd = document.createElement('div');
     dd.id=data["id"];
+    dd.style.padding='3px';
+    dd.style.width='100%';
     var d = document.createElement('div');
-    if(data['bk']=='1') d.style.background='#26272c';
-    d.style.borderRadius='8px';
-    d.style.boxShadow='0px 3px 0px 0px inset #ffffff05, 0 0 10px 0px #00000021, 0px -3px 1px 0px inset #00000010';
+    if(data['bk']=='1') {
+        d.style.background='#26272c';
+        d.style.borderRadius='8px';
+        d.style.boxShadow='0px 3px 0px 0px inset #ffffff05, 0 0 10px 0px #00000021, 0px -3px 1px 0px inset #00000010';
+    }
     d.style.width='100%';
     //d.style.position='absolute';
-    d.innerHTML+=`<div><div>${data['label']}</div></div>`;
-    d.innerHTML+="<div style='display: flex;align-items: center;justify-content: center;'>"+this.getWHtml(data, this.EventFunc)+"</div>";
+    d.style.padding='1px';
+    d.innerHTML+=`<div style='border:3px solid transparent;color:#919191;'><div>${data['label']}</div></div>`;
+    d.innerHTML+=`<div style='display: flex;align-items: center;justify-content: center;border:3px solid transparent'>${this.getWHtml(data, this.EventFunc)}</div>`;
     dd.appendChild(d);
     return dd;
   }
--- hubui/ui_els.js
+++ hubui/ui_els.js
@@ -1,5 +1,6 @@
 UI_Els = {
-  "btn": '`<button onclick=\'${EF}("${data["id"]}")\'>${data["id"]}</button>`',
+  "btn": '`<button class="uibtn" onclick=\'${EF}("${data["id"]}")\'>${data["text"]}</button>`',
+  "btn": '`<button class="uibtn" onclick=\'${EF}("${data["id"]}")\'>${data["text"]}</button>`',
   "text": '`<div onclick=\'${EF}("${data["id"]}")\'>${data["text"]}</div>`',
-  "col": 'var elsh=[];var elsj=data["text"];var els="";for(let i=0;i<elsj.length;i++){elsh[i]=this.addElHtml(JSON.stringify(elsj[i])).getInnerHTML()};for(let i=0;i<elsh.length;i++){els+=elsh[i]};`<div style="width:100%;display:flex" onclick=\'${EF}("${data["id"]}")\'>${els}</div>`',
+  "col": 'var elsj=data["text"];var els="";for(let i=0;i<elsj.length;i++){elsj[i]["p"]="0";els+=this.addElHtml(JSON.stringify(elsj[i])).outerHTML};`<div style="width:100%;display:flex" onclick=\'${EF}("${data["id"]}")\'>${els}</div>`',
 }
